09a63c89bf8aba84e28b2257556d02a4ee9c428f
Tonvin Tian <itonvin@gmail.com>
2018-06-21 11:50:36 +0800

    Fix baidu filter error

--- tampermonkey.baidu.js
+++ tampermonkey.baidu.js
@@ -1,15 +1,19 @@
-// ==UserScript==
-// @name         New Userscript
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        https://www.baidu.com/s*
-// @grant        none
-// ==/UserScript==
+ // ==UserScript==
+ // @name         New Userscript
+ // @namespace    http://tampermonkey.net/
+ // @version      0.1
+ // @description  try to take over the world!
+ // @author       You
+ // @match        https://www.baidu.com/*
+ // @grant        none
+ // ==/UserScript==
 
 (function() {
-    'use strict';
-    var the_interval = setInterval(function(){ document.getElementById('content_right').style.display = 'none';}, 500);
-    //setTimeout(function(){ window.clearInterval(the_interval);}, 5000);
-})();
+ 'use strict';
+ var the_interval = setInterval(function(){
+         if (  typeof document.getElementById('content_right') === 'object' ) {
+         document.getElementById('content_right').style.display = 'none';
+         }
+ }, 500);
+ //setTimeout(function(){ window.clearInterval(the_interval);}, 5000);
+ })();
